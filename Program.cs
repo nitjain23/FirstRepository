68655df28b814b61b91754aa836cd661d244b140
nitjain23 <nitjain23@outlook.com>
2024-08-21 19:06:35 +0530

    Add Project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace dotnet_core_hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LogMsg();
+        }
+
+        private async static Task LogMsg()
+        {
+            Console.WriteLine("Hello Nitin Jain. How are you?");
+        }
+    }
+}
